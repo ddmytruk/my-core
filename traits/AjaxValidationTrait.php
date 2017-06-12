b23f67b7cbf543c97ee1cd6e5f72c14100d0e9c3
Dmytro Dmytruk <d.dmytruk@icloud.com>
2017-06-12 16:19:40 +0300

    base

---
+++ traits/AjaxValidationTrait.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmytrodmytruk
+ * Date: 12.06.17
+ * Time: 16:15
+ */
+
+namespace ddmytruk\traits;
+
+use yii\base\Model;
+use yii\web\Response;
+use yii\widgets\ActiveForm;
+
+trait AjaxValidationTrait
+{
+    /**
+     * Performs ajax validation.
+     *
+     * @param Model $model
+     *
+     * @throws \yii\base\ExitException
+     */
+    protected function performAjaxValidation(Model $model)
+    {
+        if (\Yii::$app->request->isAjax && $model->load(\Yii::$app->request->post())) {
+            \Yii::$app->response->format = Response::FORMAT_JSON;
+            \Yii::$app->response->data   = ActiveForm::validate($model);
+            \Yii::$app->response->send();
+            \Yii::$app->end();
+        }
+    }
+}
